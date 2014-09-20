cc94b89d9283d90f091425b244944fc3850ff8ea
Olen <olendvcook@gmail.com>
2014-09-20 15:10:46 -0500

    ascii art written to a file

--- src/main.py
+++ src/main.py
@@ -1,14 +1,43 @@
-from PIL import Image
-
-class Ascii_File:
-    def __init__(self, filename):
-        print 'File name - ' + filename
-		
-        self.imageHandler = Image.open(filename)
-		
-		
+from PIL import Image	
+from PIL import ImageOps	
 	
-x = Ascii_File('resources/picture.png')
+im = Image.open('../resources/picture.png')
 
-print x.imageHandler.size
+print im.size
 
+im = ImageOps.grayscale(im)
+
+im = im.resize((100,100), Image.ANTIALIAS)
+
+#im.show()
+
+fh = open('asciidata', 'w')
+
+
+
+for y in range (0, im.size[1]):
+	for x in range (0, im.size[0]):
+		lum =  im.getpixel((x,y))
+		if lum <= 29:
+			fh.write('@')
+		elif lum <= 67:
+			fh.write('#')
+		elif lum <= 85:
+			fh.write('8')
+		elif lum <= 113:
+			fh.write('&')
+		elif lum <= 141:
+			fh.write('o')
+		elif lum <= 169:
+			fh.write(':')
+		elif lum <= 197:
+			fh.write('*')
+		elif lum <= 225:
+			fh.write(',')
+		elif lum <= 255:
+			fh.write(' ')	
+	fh.write('\n')
+		
+		
+
+fh.close()
