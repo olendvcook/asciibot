b3b9c164163b8e220aeb6946ba3eb15d419d5853
Olen <olendvcook@gmail.com>
2014-09-20 17:42:49 -0500

    alternate ascii characters for a cleaner less detailed look

--- src/main.py
+++ src/main.py
@@ -7,7 +7,11 @@ print im.size
 
 im = ImageOps.grayscale(im)
 
-im = im.resize((100,100), Image.ANTIALIAS)
+im = im.resize((120,65), Image.ANTIALIAS)
+
+#so the ascii looked really good when zoomed out so i wanted to keep it as an option
+#but it does look better with less characters when not zoomed in
+extraascii = False
 
 #im.show()
 
@@ -16,26 +20,42 @@ fh = open('asciidata', 'w')
 
 
 for y in range (0, im.size[1]):
-	for x in range (0, im.size[0]):
+	fh.write('    ')
+	for x in range (0, im.size[0]):	
 		lum =  im.getpixel((x,y))
-		if lum <= 29:
-			fh.write('@')
-		elif lum <= 67:
-			fh.write('#')
-		elif lum <= 85:
-			fh.write('8')
-		elif lum <= 113:
-			fh.write('&')
-		elif lum <= 141:
-			fh.write('o')
-		elif lum <= 169:
-			fh.write(':')
-		elif lum <= 197:
-			fh.write('*')
-		elif lum <= 225:
-			fh.write(',')
-		elif lum <= 255:
-			fh.write(' ')	
+		if extraascii :
+			if lum <= 29:
+				fh.write('@')
+			elif lum <= 67:
+				fh.write('#')
+			elif lum <= 85:
+				fh.write('8')
+			elif lum <= 113:
+				fh.write('&')
+			elif lum <= 141:
+				fh.write('o')
+			elif lum <= 169:
+				fh.write(':')
+			elif lum <= 197:
+				fh.write('*')
+			elif lum <= 225:
+				fh.write(',')
+			elif lum <= 255:
+				fh.write(' ')
+			
+		else:
+			if lum <= 51:
+				fh.write('@')
+			elif lum <= 106:
+				fh.write('8')
+			elif lum <= 157:
+				fh.write('o')
+			elif lum <= 183:
+				fh.write(':')
+			elif lum <= 204:
+				fh.write("'")
+			else:
+				fh.write(' ')
 	fh.write('\n')
 		
 		
