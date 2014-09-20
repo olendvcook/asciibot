1a4ad2903b592083730d4c52136b437ee3bf4f0c
Olen <olendvcook@gmail.com>
2014-09-20 14:04:28 -0500

    moved resources directory, got image loading working

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
-import Image
+from PIL import Image
 
 class Ascii_File:
     def __init__(self, filename):
         print 'File name - ' + filename
+		
+        self.imageHandler = Image.open(filename)
+		
+		
 	
 x = Ascii_File('resources/picture.png')
+
+print x.imageHandler.size
 
