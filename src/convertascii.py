4a59cb0e4552052306f779c30fde5a67fd7640c3
Forrest <forrest.town18@gmail.com>
2014-09-21 09:18:05 -0700

    changed the name from main to convertascii. added function. cleaned comments.

---
+++ src/convertascii.py
@@ -0,0 +1,65 @@
+from PIL import Image
+from PIL import ImageOps
+
+def create_ascii():
+	# Open the image
+	im = Image.open('../resources/picture.png')
+
+	#Grey the image
+	im = ImageOps.grayscale(im)
+
+	# This size puts us at 8125 characters and a good size for
+	# reddit comments
+	im = im.resize((120,55), Image.ANTIALIAS)
+	
+	# Use more characters?
+	extraascii = False
+
+	# Open ASCII file, fh is handler
+	fh = open('asciidata', 'w')
+
+	# Go through picture's pixels, check luminosity, and print
+	# out a character
+	for y in range (0, im.size[1]):
+		fh.write('    ')
+		for x in range (0, im.size[0]):
+			lum =  im.getpixel((x,y))
+			if extraascii :
+				if lum <= 29:
+					fh.write('@')
+				elif lum <= 67:
+					fh.write('#')
+				elif lum <= 85:
+					fh.write('8')
+				elif lum <= 113:
+					fh.write('&')
+				elif lum <= 141:
+					fh.write('o')
+				elif lum <= 169:
+					fh.write(':')
+				elif lum <= 197:
+					fh.write('*')
+				elif lum <= 225:
+					fh.write(',')
+				elif lum <= 255:
+					fh.write(' ')
+			
+			else:
+				if lum <= 38:
+					fh.write('@')
+				elif lum <=75:
+					fh.write('&')
+				elif lum <= 98:
+					fh.write('o')
+				elif lum <= 157:
+					fh.write("|")
+				elif lum <= 210:
+					fh.write(":")
+				elif lum <= 224:
+					fh.write(".")
+				else:
+					fh.write(' ')
+		fh.write('\n')
+
+	# Close the file handler
+	fh.close()
