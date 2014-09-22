6a31956779fc67454ba4733adaa6626e7b4ee978
Olen <olendvcook@gmail.com>
2014-09-22 00:01:20 -0500

    added todo/ideas

--- resources/codesnippits.py
+++ resources/codesnippits.py
@@ -1,2 +1,17 @@
 #stick random code to save in here
 #or notes
+
+#TODO: /IDEAS
+
+#delete comment if downvoted enough
+
+#post results to r/ascii_art
+
+#Error handling, recovering
+
+#ascii gifs
+
+#better aspect ratio handling
+
+#better ascii generation
+
